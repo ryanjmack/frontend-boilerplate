f658dcb5d9b835829464ba3a9528e1c93b8bafbe
Ryan <ryanjmack21@gmail.com>
2018-07-08 07:28:47 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+
+gulp.task('default', ['sass', 'serve']);
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: './src'
+        }
+    });
+
+    gulp.watch('./src/stylesheets/scss/**/*.scss', ['sass']);
+    gulp.watch('./src/js/**/*.js').on('change', browserSync.reload);
+    gulp.watch('./src/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+    gulp.src('./src/stylesheets/scss/master.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./src/stylesheets/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
---
+++ src/js/master.js
@@ -0,0 +1,1 @@
+console.log("master.js is linked properly!")
